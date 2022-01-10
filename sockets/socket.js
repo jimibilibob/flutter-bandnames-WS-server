73214de6cd1d903f55224f3bb835b91cbef089a3
Israel Aguilar <isra.neri2@gmail.com>
2022-01-10 12:38:20 -0400

    Chages on bands finished

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,19 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands;
+
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Moderato'));
+bands.addBand( new Band('Black Eyed Peace'));
+bands.addBand( new Band('Avicci'));
 
 // Socket messages
 io.on('connection', client => {
-  // client.on('event');
+  
+  client.emit('active-bands', bands.getBands());
+
   console.log('Client connected');
   client.on('disconnect',  () => {
     console.log('Disconnected client!');
@@ -11,5 +22,26 @@ io.on('connection', client => {
   client.on('message', (payload) => {
     console.log(`Message from ${payload.name}`);
     io.emit('message', {admin: 'New message received!!'});
+  });
+
+  client.on('emit-message', (payload) => {
+    console.log(`Message from ${payload.name}`);
+    // io.emit('new-message', {admin: payload}); // emist to everybody
+    client.broadcast.emit('new-message', {admin: payload}); // emist to everybody but you
+  });
+
+  client.on('vote-band', (payload) => {
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('add-band', (payload) => {
+    bands.addBand(new Band(payload.name));
+    io.emit('active-bands', bands.getBands());
+  });
+
+  client.on('delete-band', (payload) => {
+    bands.deleteBand(new Band(payload.name));
+    io.emit('active-bands', bands.getBands());
   });
 });
